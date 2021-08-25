a932b00ac1aa0815ec7ad6ef53874f211d9e3a2f
Izadora Sant'Anna <santannaizadora@gmail.com>
2021-08-25 00:07:25 -0300

    delete product

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { ProductCrudComponent } from "./views/product-crud/product-crud.componen
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
+  
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+
 
 const routes: Routes = [ {
   path: "",
@@ -22,6 +25,10 @@ const routes: Routes = [ {
 {
   path: "products/update/:id",
   component: ProductUpdateComponent
+},
+{
+  path: "products/delete/:id",
+  component: ProductDeleteComponent
 }];
 
 @NgModule({
